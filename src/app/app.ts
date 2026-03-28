5ba96e321c62fa428a751b2a7a21985a881bdf70
MahendherThatikonda <116361201+MahendherThatikonda@users.noreply.github.com>
2026-03-28 23:04:43 +1300

    Added Header section and state variables

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header';
+import { HeaderComponent } from '../header/header';
+import { User } from './user/user';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, RouterOutlet],
+  imports: [HeaderComponent, User],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/user/user.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex=Math.floor(Math.random()*DUMMY_USERS.length);
+
+@Component({
+  selector: 'app-user',
+  standalone:true,
+  imports: [],
+  templateUrl: './user.html',
+  styleUrl: './user.css',
+})
+export class User {
+  selectedUser=DUMMY_USERS[randomIndex];
+
+  get imagePath(){
+    return 'users/users/'+this.selectedUser.avatar;
+  }
+  
+  onSelectuser(){
+//    console.log("clicked!");
+const randomIndex=Math.floor(Math.random()*DUMMY_USERS.length);
+this.selectedUser=DUMMY_USERS[randomIndex];
+  }
+
+}
